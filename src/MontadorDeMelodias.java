5fd9ba8dbe4898ec7777fcb5f488bcb3326f8bbe
lucas <lucas@lucas-PC>
2013-11-11 19:24:41 -0200

    fechando o painel das notas e o seletor de harmônicos

--- src/MontadorDeMelodias.java
+++ src/MontadorDeMelodias.java
@@ -1,6 +1,8 @@
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -36,6 +38,7 @@ public class MontadorDeMelodias {
 	JSpinner FrequenciaSpinner = new JSpinner();
 	JSpinner AmplitudeSpinner = new JSpinner();
 	JSpinner FaseSpinner = new JSpinner();
+	JSpinner HarmonicoSpinner = new JSpinner();
 	JButton NovaNotaBtn = new JButton("Nova Nota");
 	private final JButton btnPlay_1 = new JButton("Play 2");
 	private final JButton btnPlay_2 = new JButton("Play 3");
@@ -68,7 +71,7 @@ public class MontadorDeMelodias {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame.setBounds(100, 100, 572, 370);
+		frame.setBounds(100, 100, 698, 497);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
@@ -103,6 +106,11 @@ public class MontadorDeMelodias {
 				new Float(1)));
 		FaseSpinner.setBounds(101, 98, 39, 20);
 		frame.getContentPane().add(FaseSpinner);
+		NovaNotaBtn.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+			}
+		});
 		NovaNotaBtn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -114,7 +122,7 @@ public class MontadorDeMelodias {
 		frame.getContentPane().add(NovaNotaBtn);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(171, 11, 375, 296);
+		scrollPane.setBounds(173, 11, 486, 296);
 		frame.getContentPane().add(scrollPane);
 		scrollPane.setViewportView(TabelaNotas);
 
@@ -159,6 +167,147 @@ public class MontadorDeMelodias {
 		btnPlay_2.setBounds(10, 228, 89, 23);
 
 		frame.getContentPane().add(btnPlay_2);
+
+		JButton btnA = new JButton("A");
+		btnA.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(27.5 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnA.setBounds(10, 318, 58, 23);
+		frame.getContentPane().add(btnA);
+
+		JButton btnA_1 = new JButton("A#");
+		btnA_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(29.14 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnA_1.setBounds(10, 354, 58, 23);
+		frame.getContentPane().add(btnA_1);
+
+		JButton btnB = new JButton("B");
+		btnB.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(30.87 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnB.setBounds(10, 388, 58, 23);
+		frame.getContentPane().add(btnB);
+
+		JButton btnC = new JButton("C");
+		btnC.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(16.35 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnC.setBounds(82, 318, 58, 23);
+		frame.getContentPane().add(btnC);
+
+		JButton btnC_1 = new JButton("C#");
+		btnC_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(17.32 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnC_1.setBounds(84, 354, 56, 23);
+		frame.getContentPane().add(btnC_1);
+
+		JButton btnD = new JButton("D");
+		btnD.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(18.35 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnD.setBounds(84, 388, 56, 23);
+		frame.getContentPane().add(btnD);
+
+		JButton btnD_1 = new JButton("D#");
+		btnD_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(19.45 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnD_1.setBounds(150, 318, 56, 23);
+		frame.getContentPane().add(btnD_1);
+
+		JButton btnE = new JButton("E");
+		btnE.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(20.60 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnE.setBounds(150, 354, 58, 23);
+		frame.getContentPane().add(btnE);
+
+		JButton btnF = new JButton("F");
+		btnF.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(21.83 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnF.setBounds(148, 388, 58, 23);
+		frame.getContentPane().add(btnF);
+
+		JButton btnF_1 = new JButton("F#");
+		btnF_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(23.12 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnF_1.setBounds(216, 318, 56, 23);
+		frame.getContentPane().add(btnF_1);
+
+		JButton btnG = new JButton("G");
+		btnG.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(24.5 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnG.setBounds(218, 354, 54, 23);
+		frame.getContentPane().add(btnG);
+
+		JButton btnG_1 = new JButton("G#");
+		btnG_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FrequenciaSpinner.setValue(25.96 * Math.pow(2,
+						(int) HarmonicoSpinner.getValue()));
+			}
+		});
+		btnG_1.setBounds(216, 388, 58, 23);
+		frame.getContentPane().add(btnG_1);
+
+		HarmonicoSpinner.setModel(new SpinnerNumberModel(new Integer(4),
+				new Integer(0), null, new Integer(1)));
+		HarmonicoSpinner.setBounds(101, 290, 29, 20);
+		frame.getContentPane().add(HarmonicoSpinner);
+
+		JLabel lblHarmnico = new JLabel("Harmônico");
+		lblHarmnico.setBounds(10, 293, 81, 14);
+		frame.getContentPane().add(lblHarmnico);
 		TabelaNotas.getColumnModel().getColumn(4).setPreferredWidth(76);
 	}
 
